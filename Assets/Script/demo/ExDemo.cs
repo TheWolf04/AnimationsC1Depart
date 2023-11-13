a27ab806d93e3991980e7096fc5dc8bd3b9c28d8
TheWolf04 <mathis.tousignantp@gmail.com>
2023-11-13 11:59:35 -0500

    2

---
+++ Assets/Script/demo/ExDemo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExDemo : MonoBehaviour
+{
+
+    int _age = 29;
+    bool _verite = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("allo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Bebye");
+    }
+}
---
+++ Assets/Script/demo/OuvrirPorte.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuvrirPorte : MonoBehaviour
+{
+
+    [SerializeField] GameObject _porteDroite;
+    [SerializeField] GameObject _porteGauche;
+    [SerializeField] GameObject _Joueur;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(_porteDroite);
+        Debug.Log(_porteGauche);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            _porteGauche.GetComponent<Animator>().SetBool("Ouvre",true);
+            
+
+        }
+
+    }
+
+    
+        void OnTriggerExit(Collider other)
+       
+    {
+        if(other.tag == "Player"){
+            _porteGauche.GetComponent<Animator>().SetBool("Ouvre",false);
+            
+
+        }
+
+    }
+
+    
+}
